5a78ab5739fc5c9e00517f5190d2019954cb36c5
alamz <zafara95@gmail.com>
2017-11-29 21:09:11 -0600

    tested 2 objects - adding and dropping ref - works!

--- Assignments/A4/ObjectManager.c
+++ Assignments/A4/ObjectManager.c
@@ -48,21 +48,30 @@ void initPool()
 	memRegionStart = NULL;
 	memRegionEnd = NULL;
 	numObjects = 0;
-	currBuffer = *buffer1;
+
+	printf("\tRUNNING INITPOOL: allocate buffer1 next\n");
 	buffer1 = malloc(MEMORY_SIZE);
-	printf("RUNNING INITPOOL() - SUCCESSFULL\n");
-	//assert here
+	printf("\tALLOCATING BUFFER  - SUCCESSFULL\n");
+
+//	assert here
+	if(buffer1 == NULL)
+	{
+		assert(buffer1 != NULL);
+	}
+
 	buffer2 = malloc(MEMORY_SIZE);
 	//assert here
+	currBuffer = *buffer1;
 	printf("RUNNING INITPOOL() - SUCCESSFULL\n");
 }
 
 Ref insertObject(ulong size)
 {
-	nextRef++; //this object has at least one reference to it.
+
 	memObject * curr = memRegionStart;
 	memObject * newMemBlock = malloc(size);
 	newMemBlock->ID = nextRef;
+	nextRef++; //NEW OR THE NEXT OBJECT SHOULD HAVE THIS ID
 	newMemBlock->size = size;
 	newMemBlock->refCount = 1;
 	newMemBlock->next = NULL;
@@ -72,21 +81,88 @@ Ref insertObject(ulong size)
 		memRegionStart = newMemBlock;
 	else
 	{
-		while(curr != NULL)
+		while(curr->next != NULL)
 		{
 			curr = curr->next;
 		}
+		curr->next = newMemBlock;
+		curr = curr->next;
 		curr = newMemBlock;
 	}
 	freePtr += size;
 
-	return nextRef;
+	if(newMemBlock == NULL)
+	{
+		return (nextRef=0); //0 indicates NULL, while >0 indicates this object's id
+	}
+	printf("insertObject() new block pointer: %p\n", newMemBlock);
+	printf("insertObject() memRegionStart pointer: %p\n", memRegionStart);
+	return newMemBlock->ID;
+}
+
+void *retrieveObject( Ref ref )
+{
+	memObject * curr = memRegionStart;
+	while(curr != NULL)
+	{
+		if(curr->ID == ref)
+		{
+			return curr;
+		}
+		curr = curr->next;
+	}
+	printf("FAILED\n");
+	return NULL;
+}
+
+void addReference(Ref ref)
+{
+	memObject * curr = retrieveObject(ref);
+	curr->refCount += 1;
+	printf("ADDING REFERENCE TO ID %lu\n",ref);
+
+
+	printf("\tID: %lu has %d references\n",ref,(curr->refCount));
+	printf("ADDING REFERENCE TO ID %lu - SUCCESSFULL\n",ref);
+}
+
+void dropReference( Ref ref )
+{
+	memObject * curr = retrieveObject(ref);
+	curr->refCount--;
+	printf("DROPPING REFERENCE IN ID %lu\n",ref);
+
+
+	printf("\tID: %lu has %d references\n",ref,(curr->refCount));
+	printf("DROPPING REFERENCE IN ID %lu SUCCESSFULL\n",ref);
 }
 
 int main(void)
 {
+	printf("\nRUNNING MAIN\n");
 	initPool();
-	Ref id = insertObject(32);
-	printf("%lu \n",id);
+	Ref id = insertObject(32); //returns nextRef ID.
+	printf("\tinserted object id = %lu \n",id);
+
+	printf("\tretrieved object with id 1 %p\n",retrieveObject(id));
+
+	addReference(1);
+
+	dropReference(1);
+
+	Ref id2 = insertObject(32);
+	printf("\tinserted object id = %lu \n",id2);
+
+	printf("\tretrieved object with id %lu %p\n",id2,retrieveObject(id2));
+
+	addReference(id2);
+	addReference(id2);
+	addReference(id2);
+	addReference(id2);
+
+	dropReference(id2);
+
+
+
 }
 
